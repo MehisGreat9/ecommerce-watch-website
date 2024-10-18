896ecfe78f934433020524ea98880eb681f770d9
NerdyCodeCrafter <159589149+MehisGreat9@users.noreply.github.com>
2024-10-18 14:24:28 +0500

    Update main.js

--- main.js
+++ main.js
@@ -28,3 +28,67 @@ window.addEventListener("scroll", function (){
         backtoTop.classList.remove('show');
     }
 });
+
+// HEADER NAVBAR
+const ul = document.querySelector('.hex_navbar ul');
+const overlay = document.querySelector('.overlay');
+const icon = document.querySelector('.nav_toggle i');
+const items = ul.querySelectorAll('li');
+
+//close navbar function
+function closeNavbar(){
+    items.forEach(item => {
+        item.style.transform = 'translateY(-20px)';
+    });
+    setTimeout(() => {
+        ul.classList.remove('show');
+        overlay.classList.remove('show');
+        icon.classList.add('fa-bars');
+        icon.classList.remove('fa-close');
+    }, 500);
+}
+
+// toggle navbar
+document.querySelector('.nav_toggle').addEventListener('click', function(){
+    if (ul.classList.contains('show')){
+        closeNavbar();
+    } else {
+        ul.classList.add('show');
+        overlay.classList.add('show');
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-close');
+        items.forEach((item, index) => {
+            item.style.transform = `translateY(${index * 60}px)`;
+        });
+    }
+});
+
+//scroll to section and close navbar
+document.querySelector('.hex_navbar').addEventListener('click', function(e){
+    if(e.target.tagName === 'A'){
+        e.preventDefault();
+        const targetId = e.target.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+        setTimeout(closeNavbar, 800);
+    }
+});
+
+//close navbar on clicking outside 
+document.addEventListener('click', function(event){
+    const isClickInsideNav = event.target.closest('.hex_navbar') || 
+    event.target.closest('.nav_toggle');
+    if (!isClickInsideNav && ul.classList.contains('show')){
+        closeNavbar();
+    }
+})
+
+// HOME SECTION SLIDER
+document.getElementById('next').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').appendChild(lists[0]);
+}
+document.getElementById('prev').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').prepend(lists[lists.length - 1]);
+}
